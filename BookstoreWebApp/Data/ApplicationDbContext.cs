7726551926b486aefad50a4780f423b99c04f4a8
Hesham Omar <hesham.omar1122@gmail.com>
2023-04-15 01:35:10 +0200

    Add project files.

---
+++ BookstoreWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using BookstoreWebApp.Models;
+
+namespace BookstoreWebApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<BookstoreWebApp.Models.Book> Book { get; set; } = default!;
+    }
+}
